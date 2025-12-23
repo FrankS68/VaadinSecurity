cf2e09a818719ebc3e619cd7358aa3c8bf12cb49
start.vaadin.com <no-reply@vaadin.com>
2025-12-23 07:30:10 +0000

    Generated project

---
+++ src/main/java/consulting/segieth/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package consulting.segieth.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/consulting/segieth/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package consulting.segieth.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
